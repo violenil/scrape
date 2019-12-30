39caf439db359170d6f3163c8dca468bf06c6782
violenil <retrospect@protonmail.com>
2019-12-30 21:00:42 +0200

    first commit for my code

---
+++ wikipy.py
@@ -0,0 +1,68 @@
+import sys 
+import requests  #to download
+import bs4  #to extract data
+import csv
+
+res = requests.get('https://en.wikipedia.org/wiki/' + ' '.join(sys.argv[1:]))
+res.raise_for_status() # to check for errors
+
+wiki = bs4.BeautifulSoup(res.text, "html.parser")   #res.text is text from page, html.parser will help structure text into html format
+
+spaceXheading = ""
+for i in wiki.select('h1'):
+    spaceXheading += i.getText()
+
+spaceXbody = ""
+for i in wiki.select('p'):
+    spaceXbody += i.getText()
+    #print(i.getText())
+
+#print(spaceXheading)
+#print(spaceXbody)
+
+pagesToVisit = []
+
+count = 0
+repElemList = wiki.find_all('a')
+for repElem in repElemList:
+    repElemHref = repElem.get('href')
+    if repElemHref:
+        if 'cite' in repElemHref:
+            pass
+        elif repElemHref[:1] == "#":
+            pass
+        elif repElemHref[-12:] == '/wiki/SpaceX':
+            pass
+        elif 'youtube' in repElemHref:
+            pass
+        else:
+            if repElemHref[:1] == '/':
+                repElemHref = 'https://en.wikipedia.org' + repElemHref
+            if repElemHref not in repElemList:
+                pagesToVisit.append(repElemHref)
+                #print(repElemHref)
+                #print(" ")
+                count += 1
+print("Count: " + str(count))
+print(pagesToVisit)
+print(str(len(pagesToVisit)))
+
+with open('spaceX.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow([spaceXheading, spaceXbody])
+    for page in pagesToVisit:
+        try:
+            r = requests.get(page)
+            print(page)
+        except:
+            print("Error")
+            print(type(r.raise_for_status()))
+            pass
+        soup = bs4.BeautifulSoup(r.text, "html.parser")
+        body = ""
+        for i in soup.select('p'):
+            body += i.getText()
+        heading = ""
+        for i in soup.select('h1'):
+            heading += i.getText()
+            writer.writerow([heading, body])
